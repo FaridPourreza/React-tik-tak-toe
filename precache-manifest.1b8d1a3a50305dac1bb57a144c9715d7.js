bf9f752ef68e10e46c419bedb7b543f1eebbec28
unknown <pourrezafarid447@yahoo.com>
2020-07-10 00:56:43 +0430

    Updates

---
+++ precache-manifest.1b8d1a3a50305dac1bb57a144c9715d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feb2afd888fa9d6b457f45fa92a725ab",
+    "url": "/React-tik-tak-toe/index.html"
+  },
+  {
+    "revision": "9ae24d453bbed23207bd",
+    "url": "/React-tik-tak-toe/static/js/2.2bc05396.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-tik-tak-toe/static/js/2.2bc05396.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ae59f5aa536f5e4286a",
+    "url": "/React-tik-tak-toe/static/js/main.48c9453d.chunk.js"
+  },
+  {
+    "revision": "d999c4dae54457dd3ed5",
+    "url": "/React-tik-tak-toe/static/js/runtime-main.587c467b.js"
+  }
+]);
